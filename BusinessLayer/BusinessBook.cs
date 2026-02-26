ea04e45b26ba641b0f892acf631f186c55b56dff
jankocar <jjjankojjj@gmail.com>
2026-02-26 12:50:43 +0100

    Update BusinessBook.cs

--- BusinessLayer/BusinessBook.cs
+++ BusinessLayer/BusinessBook.cs
@@ -15,7 +15,7 @@ namespace BusinessLayer
 
         public BusinessBook(IBookRepository bookRepository)
         {
-            this.bookRepository = bookRepository;
+            this.bookRepository = bookRepository;             // dovde uvek
         }
 
         public List<Book> GetBooksWith50()
@@ -46,5 +46,64 @@ namespace BusinessLayer
             }
             return "Greska!";
         }
-    }
+
+        public List<Car> GetCarsSortedByPrice()
+        {
+            return carRepository.GetCars()
+                .OrderBy(c => c.Price)
+                .ToList();
+        }
+        
+        public List<Car> GetCarsSortedByPriceDesc()
+        {
+            return carRepository.GetCars()
+                .OrderByDescending(c => c.Price)
+                .ToList();
+        }
+        
+        public List<Car> GetCarsNewerThan(int year)
+        {
+            return carRepository.GetCars()
+                .Where(c => c.Year > year)
+                .ToList();
+        }
+        
+        public List<Car> GetCarsUnderPrice(decimal max)
+        {
+            return carRepository.GetCars()
+                .Where(c => c.Price < max)
+                .ToList();
+        }
+        
+        public string InsertCar(Car car)
+        {
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(car, null, null);
+        
+            if (!Validator.TryValidateObject(car, validationContext, validationResults, true))
+            {
+                return string.Join(", ", validationResults.Select(vr => vr.ErrorMessage));
+            }
+        
+            if (carRepository.InsertCar(car))
+                return "Uspesno!";
+        
+            return "Greska!";
+        }
+        
+        public bool DeleteCar(int id)
+        {
+            return carRepository.DeleteCar(id);
+        }
+        
+        public bool UpdateCar(Car car)
+        {
+            return carRepository.UpdateCar(car);
+        }
+        
+        public Car? GetCarById(int id)
+        {
+            return carRepository.GetCarById(id);
+        }
+            }
 }
